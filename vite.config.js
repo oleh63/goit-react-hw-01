0f58e345c0b8b765dd6286c1f5677690e3089c0e
Oleh Skrypets <olehskrupets@gmail.com>
2024-12-12 22:17:07 +0200

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    sourcemap: true,
-  }
+ plugins: [react()],
+ build: {
+  sourcemap: true,
+ }
 });
 
