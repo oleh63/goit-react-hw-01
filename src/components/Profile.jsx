a66c92e217929e20cb27b19741929300f3f1adac
Oleh Skrypets <olehskrupets@gmail.com>
2024-12-13 17:35:27 +0200

    task-1 fix

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,24 +3,24 @@ import styles from "./Profile.module.css";
 const Profile = ({ name, tag, location, image, stats }) => {
   return (
     <div className={styles.container}>
-      <div>
+      <div className={styles.container_profile}>
         <img src={image} alt="User avatar" width={250} />
-        <p className={styles.info_user}>{name}</p>
+        <p className={styles.user_name}>{name}</p>
         <p className={styles.info_user}>@{tag}</p>
         <p className={styles.info_user}>{location}</p>
       </div>
       <ul className={styles.items}>
         <li className={styles.item}>
-          <span>Followers</span>
-          <span>{stats.followers}</span>
+          <span className={styles.span_profile}>Followers</span>
+          <span className={styles.span_info}>{stats.followers}</span>
         </li>
         <li className={styles.item}>
-          <span>Views</span>
-          <span>{stats.views}</span>
+          <span className={styles.span_profile}>Views</span>
+          <span className={styles.span_info}>{stats.views}</span>
         </li>
         <li className={styles.item}>
-          <span>Likes</span>
-          <span>{stats.likes}</span>
+          <span className={styles.span_profile}>Likes</span>
+          <span className={styles.span_info}>{stats.likes}</span>
         </li>
       </ul>
     </div>
